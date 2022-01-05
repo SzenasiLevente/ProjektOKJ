825ec40d83b3c6f2b2b59b484ccccdf6e7c1cca7
SzenasiLevente <59849277+SzenasiLevente@users.noreply.github.com>
2022-01-05 08:44:44 +0100

    Index testing

--- index.php
+++ index.php
@@ -90,7 +90,10 @@
 
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h1 text-white">Dashboard</h1>
+        <h1 class="h1 text-white">General title about games here</h1>
+      </div>
+      <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 dashText">
+        <p>General info about games here</p>
       </div>
     </main>
   </div>
