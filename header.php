96ddbc059ca19a599ac9be7d2848635a89398a8a
SzenasiLevente <59849277+SzenasiLevente@users.noreply.github.com>
2022-01-30 14:19:33 +0100

    name change

--- header.php
+++ header.php
@@ -19,7 +19,7 @@ session_start();
 
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow" id="bootstrap-overrides">>
 
-  <a class="navbar-brand col-md-3 col-lg-2 px-3 py-3" href="index.php">InfoGames</a>
+  <a class="navbar-brand col-md-3 col-lg-2 px-3 py-3" href="index.php">Vapour</a>
 
   <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
