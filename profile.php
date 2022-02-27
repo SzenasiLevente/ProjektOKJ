f7959b857b91dec14499808737763a5256406ab9
SzenasiLevente <59849277+SzenasiLevente@users.noreply.github.com>
2022-02-27 20:47:57 +0100

    started on profiles

--- profile.php
+++ profile.php
@@ -1,4 +1,7 @@
-<?php require 'header.php'; ?>
+<?php 
+require 'header.php'; 
+require 'inc/inc_gamequery.php';
+?>
 
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
@@ -7,19 +10,71 @@
 
       <div class="d-flex justify-content-between flex-wrap text-white" style="font-size: 1.5rem;">
       <div class="container-fluid">
-        <div class="row">
-        <div class="col-lg-6">
-        <h3>Personal info</h3>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur iusto ab doloribus. Ratione explicabo facere atque repudiandae sit saepe officia reprehenderit modi mollitia non recusandae accusamus, corrupti quibusdam quidem placeat architecto nobis, nihil quas nesciunt natus sunt expedita. Natus, in, sint, enim quaerat culpa aliquam repellat magni dicta vitae recusandae eum. Nostrum commodi, id vel reiciendis quidem saepe numquam quae. Animi ipsa ratione dignissimos similique?</p>
+      <div class="card bg-dark my-3">
+    <div class="row">
+
+      <div class="col-md-7 px-3">
+        <div class="card-block px-6">
+          <h4 class="card-title mx-5 my-5"><?php echo ''.$_SESSION["username"].''?></h4>
+          <p class="card-text">
+        <?php
+        while ($row = mysqli_fetch_assoc($descresult)){
+            echo'<p class="mx-5">'.$row["gameDesc"].'</p>
+            <button class="btn btn-light mx-5 my-5">Get the game here</button>';
+        }
+        ?>
+          </p>
         </div>
-        <div class="col-lg-6">
-        <h3>Scores</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas assumenda porro amet corporis explicabo expedita soluta commodi repellat sequi ipsum nulla, atque alias tempore sed ratione illo exercitationem accusamus qui eaque vitae molestias magni aspernatur officia? Ducimus sint quo quia enim veniam, deleniti ipsa suscipit a recusandae ex quas quis nemo accusantium iure corporis, dolores quod.  </p>
+      </div>
+      <div class="col-md-5">
+      <?php
+              while ($row = mysqli_fetch_assoc($resultpic)){
+
+                echo'
+          <img src="IMG/'. $row["gamePic"] .'" class="img-fluid card-img rounded mx-auto d-block float-right" alt="gameone">';
+          }
+        ?>
+      </div>
+        </div>
+
+      </div>
+
+      <div class="card bg-dark my-3">
+    <div class="row">
+
+      <div class="col-md-12 px-3">
+        <div class="card-block px-6">
+          <h4 class="card-title mx-5 my-5">Scores</h4>
+          <div class="table-responsive">
+          <table class="table table-striped table-light text-center">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Username</th>
+      <th scope="col">Points</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php
+  $place = 0;
+  while ($row = mysqli_fetch_assoc($leaderresult)){
+
+    echo'
+    <tr>
+      <th scope="row">'.strval($place = $place+1).'</th>
+      <td>'.$row["userName"].'</td>
+      <td>'.$row["aScorePoints"].'</td>
+    </tr>';
+  }
+  ?>
+  </tbody>
+</table>
+</div>
         </div>
       </div>
         </div>
       </div>
-      </div>
+    </div>
     </main>
 
 <?php require 'footer.php'; ?>
