d3d2769ae4bde9a3b4f8776b7c3fd3d345e60f0e
SzenasiLevente <59849277+SzenasiLevente@users.noreply.github.com>
2022-02-28 17:27:38 +0100

    added games and scores to profile

---
+++ inc/inc_profquery.php
@@ -0,0 +1,25 @@
+<?php
+require "inc_connection.php";
+
+$games = 'SELECT `games`.`gameName`
+FROM `users`
+    LEFT JOIN `owngames` ON `owngames`.`ownUId` = `users`.`userId`
+    LEFT JOIN `games` ON `owngames`.`ownGId` = `games`.`gameId`
+WHERE `users`.`userName` ="'.$_SESSION["username"].'"';
+
+$resultgames = mysqli_query($connection,$games);
+
+$scores = 'SELECT `ahighscores`.`aScorePoints`, `users`.`userId`, `games`.`gameName`
+FROM `users`
+    LEFT JOIN `ahighscores` ON `ahighscores`.`aUId` = `users`.`userId`
+    LEFT JOIN `games` ON `ahighscores`.`aGId` = `games`.`gameId`
+    WHERE `users`.`userName` ="'.$_SESSION["username"].'"
+    LIMIT 5';
+
+$resultscores = mysqli_query($connection,$scores);
+
+$profilepic = 'SELECT `users`.`userPicture`
+WHERE `users`.`userName` ="'.$_SESSION["username"].'"';
+
+$respic = mysqli_query($connection,$profilepic);
+
--- profile.php
+++ profile.php
@@ -1,11 +1,11 @@
 <?php 
 require 'header.php'; 
-require 'inc/inc_gamequery.php';
+require 'inc/inc_profquery.php';
 ?>
 
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h1 text-white ">Profile</h1>
+        <h1 class="h1 text-white "><?php echo ''.$_SESSION["username"].' Users Profile'?></h1>
       </div>
 
       <div class="d-flex justify-content-between flex-wrap text-white" style="font-size: 1.5rem;">
@@ -15,23 +15,31 @@ require 'inc/inc_gamequery.php';
 
       <div class="col-md-7 px-3">
         <div class="card-block px-6">
-          <h4 class="card-title mx-5 my-5"><?php echo ''.$_SESSION["username"].''?></h4>
+          <h4 class="card-title mx-5 my-5">Owned games</h4>
           <p class="card-text">
-        <?php
-        while ($row = mysqli_fetch_assoc($descresult)){
-            echo'<p class="mx-5">'.$row["gameDesc"].'</p>
-            <button class="btn btn-light mx-5 my-5">Get the game here</button>';
-        }
-        ?>
+          <ul class="mx-5 my-5">
+          <?php
+          while ($row = mysqli_fetch_assoc($resultgames)){
+            echo'
+              <li>'.$row['gameName'].'</li>';
+          }
+            ?>
+          </ul>
           </p>
         </div>
       </div>
       <div class="col-md-5">
       <?php
-              while ($row = mysqli_fetch_assoc($resultpic)){
-
-                echo'
-          <img src="IMG/'. $row["gamePic"] .'" class="img-fluid card-img rounded mx-auto d-block float-right" alt="gameone">';
+              while ($row = mysqli_fetch_assoc($respic)){
+                if($row["userPicture"] == null)
+                {
+                  echo'
+                  <img src="IMG/def.jpg" class="img-fluid card-img rounded mx-auto d-block float-right">';
+                }
+                else{
+                  echo'
+                  <img src="IMG/'. $row["userPicture"] .'" class="img-fluid card-img rounded mx-auto d-block float-right">';
+                }               
           }
         ?>
       </div>
@@ -44,25 +52,24 @@ require 'inc/inc_gamequery.php';
 
       <div class="col-md-12 px-3">
         <div class="card-block px-6">
-          <h4 class="card-title mx-5 my-5">Scores</h4>
+          <h4 class="card-title mx-5 my-5">Your Scores</h4>
           <div class="table-responsive">
           <table class="table table-striped table-light text-center">
   <thead>
     <tr>
       <th scope="col">#</th>
-      <th scope="col">Username</th>
+      <th scope="col">Game</th>
       <th scope="col">Points</th>
     </tr>
   </thead>
   <tbody>
   <?php
   $place = 0;
-  while ($row = mysqli_fetch_assoc($leaderresult)){
-
+  while ($row = mysqli_fetch_assoc($resultscores)){
     echo'
     <tr>
       <th scope="row">'.strval($place = $place+1).'</th>
-      <td>'.$row["userName"].'</td>
+      <td>'.$row["gameName"].'</td>
       <td>'.$row["aScorePoints"].'</td>
     </tr>';
   }
